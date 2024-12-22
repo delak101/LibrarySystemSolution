875df26510d8fdc5d8fbccfb04ec4906a87ff4b6
Khaled Hesham <khaledhesham007@gmail.com>
2024-12-22 18:21:31 +0200

    Adding DLLs
    for publishing purposes

--- LibrarySystemApp/obj/Debug/net8.0/LibrarySystemApp.AssemblyInfo.cs
+++ LibrarySystemApp/obj/Debug/net8.0/LibrarySystemApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystemApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27a4eb6dc3ac2bc52334e10232f06b7a6144563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0504ef5b16c42b5daeaeeffce097b75c04d67388")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystemApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystemApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
